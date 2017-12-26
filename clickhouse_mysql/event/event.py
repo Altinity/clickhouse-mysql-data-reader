aef6f963686c68b72ee6ac698c94830a74e26364
Vladislav Klimenko <sunsingerus@gmail.com>
2017-12-26 16:53:31 +0300

    verification and debug info

--- clickhouse_mysql/event/event.py
+++ clickhouse_mysql/event/event.py
@@ -4,21 +4,29 @@
 
 class Event(object):
 
+    # main payload - one or multiple rows
+
+    # native mysql replication event
+    # one of from pymysqlreplication.row_event import
+    # contains rows internally
+    pymysqlreplication_event = None
+
+    # one row payload
+    # {'id':1, 'col1':1}
+    row = None
+
+    # multi-rows payload
+    # [{'id': 1, 'col1':1}, {'id': 2, 'col1': 2}, {'id': 3, 'col1': 3}]
+    rows = None
+
+    # additional meta-information
+    # source-dependent
+
     # db name
     schema = None
 
     # table name
     table = None
-
-    #for row in mysql_event.rows:
-    #    event.rows.append(row['values'])
-    mysql_event = None
-
-    # {'id':1, 'col1':1}
-    row = None
-
-    # [{'id': 1, 'col1':1}, {'id': 2, 'col1': 2}, {'id': 3, 'col1': 3}]
-    rows = None
 
     # /path/to/csv/file.csv
     filename = None
@@ -26,31 +34,121 @@ class Event(object):
     # ['id', 'col1', 'col2']
     fieldnames = None
 
+    # payload rows iterator
     _iter = None
 
     def __iter__(self):
-        if self.mysql_event is not None:
-            self._iter = iter(self.mysql_event.rows)
+        # create payload rows iterator
+
+        if self.pymysqlreplication_event is not None:
+            # we have native replication event - would iterate over its rows
+            self._iter = iter(self.pymysqlreplication_event.rows)
 
         elif self.row is not None:
+            # we have one row - would iterate over list of one row
             self._iter = iter([self.row])
 
         else:
+            # assume multiple rows - would iterate over them
             self._iter = iter(self.rows)
 
         return self
 
     def __next__(self):
+        # fetch next item from iterator
+
         item = next(self._iter)
 
-        if self.mysql_event is not None:
+        if self.pymysqlreplication_event is not None:
+            # in native replication event actual data are in row['values'] dict item
             return item['values']
         else:
+            # local-kept data
             return item
 
+    def verify(self):
+        # verify Event has correct data structure
+
+        if self.pymysqlreplication_event is not None:
+            # have native replication event must have some data
+            # must have non-empty list of rows with 'value'
+            # data item of reasonable len
+            if (self.pymysqlreplication_event.rows is not None) \
+                    and isinstance(self.pymysqlreplication_event.rows, list) \
+                    and (len(self.pymysqlreplication_event.rows) > 0) \
+                    and ('values' in self.pymysqlreplication_event.rows[0]) \
+                    and (len(self.pymysqlreplication_event.rows[0]['values']) > 0):
+                return True
+            else:
+                return False
+
+        if self.row is not None:
+            # one row of data must be of a reasonable len
+            if isinstance(self.row, dict) and (len(self.row) > 0):
+                return True
+            else:
+                return False
+
+        if self.rows is not None:
+            # rows of data must contain list of dicts
+            if isinstance(self.rows, list) \
+                    and (len(self.rows) > 0) \
+                    and isinstance(self.rows[0], dict) \
+                    and (len(self.rows[0]) > 0):
+                return True
+            else:
+                return False
+
+        # no data available?
+        return False
+
+    def meta(self):
+        # meta info
+
+        meta = ''
+        if self.pymysqlreplication_event is not None:
+            meta += ' mysql_event set'
+            if (self.pymysqlreplication_event.rows is not None):
+                meta += ' mysql_event.rows set'
+            if isinstance(self.pymysqlreplication_event.rows, list):
+                meta += ' mysql_event.rows is a list'
+            meta += ' len(mysql_event.rows)=' + len(self.pymysqlreplication_event.rows)
+            if (len(self.pymysqlreplication_event.rows) > 0) \
+                and ('values' in self.pymysqlreplication_event.rows[0]):
+                meta += ' mysql_event.rows[0][values] is set'
+
+                if len(self.pymysqlreplication_event.rows[0]['values']) > 0:
+                    meta += ' len(mysql_event.rows[0][values])=' + len(self.pymysqlreplication_event.rows[0]['values'])
+        else:
+            meta += ' mysql_event not set'
+
+        if self.row is not None:
+            meta += ' row set'
+            if isinstance(self.row, dict):
+                meta += ' is dict len()=' + len(self.row)
+            else:
+                meta += ' is not a dict'
+        else:
+            meta += ' row not set'
+
+        if self.rows is not None:
+            meta += ' rows set'
+            if isinstance(self.rows, list):
+                meta += ' rows is a list len(rows)=' + len(self.rows)
+                if (len(self.rows) > 0) and isinstance(self.rows[0], dict):
+                    meta += ' with dict() len(dict)=' + len(self.rows[0])
+            else:
+                meta += ' rows is not a list'
+        else:
+            meta += ' rows not set'
+
+        return meta
+
     def column_names(self):
-        if self.mysql_event is not None:
-            return self.mysql_event.rows[0]['values'].keys()
+        # fetch column names from data
+
+        if self.pymysqlreplication_event is not None:
+            return self.pymysqlreplication_event.rows[0]['values'].keys()
 
         if self.row is not None:
             return self.row.keys()
