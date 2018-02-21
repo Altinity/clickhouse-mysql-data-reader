d50b7ab7152d804a70906687d5daac54ba8fa7d5
Vladislav Klimenko <sunsingerus@gmail.com>
2018-02-21 13:26:18 +0300

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="clickhouse-mysql",
 
     # version should comply with PEP440
-    version='0.0.2018012602',
+    version='0.0.2018022101',
 
     description='MySQL to ClickHouse data migrator',
     long_description='MySQL to ClickHouse data migrator',
