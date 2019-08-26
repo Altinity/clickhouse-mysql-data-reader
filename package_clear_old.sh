54830b3ece162c20bc45055959b4521f17b85cae
Vladislav Klimenko <sunsingerus@gmail.com>
2019-08-26 13:09:36 +0300

    env: enhance dev scripts

--- package_clear_old.sh
+++ package_clear_old.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-TO_DEL="build dist clickhouse_mysql.egg-info deb_dist"
+# List of items (files and folders) to be deleted.
+# These items are package-related
+ITEMS_TO_DEL="
+build
+dist
+clickhouse_mysql.egg-info
+deb_dist
+"
 
 echo "########################################"
 echo "### Clear all build and release data ###"
 echo "########################################"
 
-echo "Deleting:"
-for DEL in $TO_DEL; do
-    echo "    $DEL"
+echo "About to delete:"
+DEL=""
+for ITEM in ${ITEMS_TO_DEL}; do
+    echo "    ${ITEM}"
+    DEL="${DEL} ${ITEM}"
 done
 
-echo "rm -rf $TO_DEL"
-rm -rf $TO_DEL
+if [[ -z "${DEL}" ]]; then
+    echo "No items to delete"
+else
+    echo "rm -rf ${DEL}"
+    rm -rf ${DEL}
+fi
