5c5148a61dfb6c0afdf971aa76cb92af9424e158
Vladislav Klimenko <sunsingerus@gmail.com>
2019-08-26 13:36:39 +0300

    env: enhance RPM and deb packager

--- package_distr_rpm.sh
+++ package_distr_rpm.sh
@@ -2,11 +2,22 @@
 
 ./package_clear_old.sh
 
+echo "##########################"
+echo "### Build RPM packages ###"
+echo "##########################"
+
 python3 setup.py bdist_rpm --packager="Vladislav Klimenko <sunsingerus@gmail.com>"
 # --spec-only
 
-# ls -l ./build/bdist.linux-x86_64/rpm/SPECS/
-# ls -l ./dist/
+echo ""
+echo ""
+echo ""
+echo "######################################"
+echo "### Results - .spec and .rpm files ###"
+echo "######################################"
+ls -la ./build/bdist.linux-x86_64/rpm/SPECS/*.spec
+ls -la ./dist/*.rpm
+
 # build RPMs with
 # rpmbuild -ba ./build/bdist.linux-x86_64/rpm/SPECS/clickhouse-mysql.spec
 
